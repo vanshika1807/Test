99ccb6b47f18fa172d85f8f64f912d3671edea63
vanshika1807 <vanshika18072001@gmail.com>
2024-10-01 16:41:01 +0530

    changes commited

--- mutliply.java
+++ mutliply.java
@@ -2,6 +2,7 @@ public class mutliply {
     public static void main(String[] args){
         int a = 4;
         int b =6;
+        System.out.println("Helloe thee test");
         System.out.println("the product is :" + (a+b));
     }
     
