f9de971e0d52ccbaae8a5326163954d2764f5017
vanshika1807 <vanshika18072001@gmail.com>
2024-10-01 14:27:39 +0530

    made some changes in sum.java

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ public class sum{
     public static void main(String[] args){
         int a = 4;
         int b =6;
-        System.out.println(a+b);
+        System.out.println("The sum is " (a+b));
     }
 }
